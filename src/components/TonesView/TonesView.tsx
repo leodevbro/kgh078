c56bcd6748699a0f57d8c6d7686e5548611a21b4
Levan Katsadze <leodevbro@gmail.com>
2022-05-19 18:40:46 +0400

    init 6

--- src/components/TonesView/TonesView.tsx
+++ src/components/TonesView/TonesView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 // import ReactDOM from "react-dom";
 // import ReactDOM from 'react-dom';
@@ -10,12 +10,12 @@ import style from "./TonesView.module.scss";
 import pathOfMeadowEsterno from "src/styling-constants/raster-items/meadow_esterno.png";
 import pathOfPlank1 from "src/styling-constants/raster-items/plank1.png";
 
-
-
 export const TonesView: React.FC<{
   className?: string;
 }> = ({ className }) => {
   const { t } = useTranslation();
+
+  const [selectedToneIndex, setSelectedToneIndex] = useState(0);
 
   return (
     <div className={cla(style.ground, className)}>
@@ -23,9 +23,28 @@ export const TonesView: React.FC<{
         <div className={style.title}>{t("woodTonesAndWarmVibes")}</div>
         <div className={style.descAndFrost}>
           <div className={style.desc}>{t("thePlanxCollectionEffortlesslyCaptures")}</div>
-          <div className={style.frost}></div>
+          <div className={style.frost}>
+            <div className={style.frostWrap}>
+              <div className={style.textOfFrost}>{t("frost")}</div>
+              <div className={style.toneBoxes}>
+                {[0, 1, 2, 3, 4].map((item, index) => {
+                  const isSelected = index === selectedToneIndex;
+                  const cl_selected = isSelected ? style.selected : "";
+
+                  return (
+                    <div key={index} className={cla(style.toneBox, cl_selected)}>
+                      <div
+                        className={style.innerBox}
+                        onClick={() => setSelectedToneIndex(index)}
+                      ></div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={style.visuals}>
+        <div className={cla(style.visuals, style[`tone${selectedToneIndex}`])}>
           <div className={style.left}>
             <img className={style.meadowImage} alt="tone view" src={pathOfMeadowEsterno} />
           </div>
--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ ReactDOM.render(
       </BroRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById("rootoflanding01"),
 );
 
 // If you want your app to work offline and load faster, you can change
