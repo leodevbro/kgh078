de4d0f0888e9cc99c34e9e400a7830bbb98c8dda
Levan Katsadze <leodevbro@gmail.com>
2022-05-25 01:49:36 +0400

    init 36

--- src/imglinks.ts
+++ src/imglinks.ts
@@ -1,192 +1,78 @@
-export const n1_sample_nest_2x =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_nest_2x.png";
-export const n2_sample_nest =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_nest.png";
-export const n3_sample_meadow =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_meadow.png";
-export const n4_sample_frost =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_frost.png";
-export const n5_sample_meadow_2x =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_meadow_2x.png";
-
-//
-
-export const n6_sample_frost_2x =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_frost_2x.png";
-export const n7_residential_2_2x =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_2_2x.jpg";
-export const n8_sample_bark_2x =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_bark_2x.png";
-export const n9_sample_bark =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_bark.png";
-export const n10_sample_acorn =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_acorn.png";
-
-//
-
-export const n11_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_1_2x.jpg";
-export const n12_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_acorn_2x.png";
-export const n13_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_2.jpg";
-export const n14_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_1.jpg";
-export const n15_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/home_banner_2x.jpg";
-
-//
-
-export const n16_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/home_banner.jpg";
-export const n17_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_7_2x.jpg";
-export const n18_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_8_2x.jpg";
-export const n19_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_8.jpg";
-export const n20_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_7.jpg";
-
-//
-
-export const n21_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_5_2x.jpg";
-export const n22_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_6.jpg";
-export const n23_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_6_2x.jpg";
-export const n24_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_3_2x.jpg";
-export const n25_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_3.jpg";
-
-export const n26_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_5.jpg";
-export const n27_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_4_2x.jpg";
-export const n28_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_4.jpg";
-export const n29_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_2.jpg";
-export const n30_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_2_2x.jpg";
-
-//
-
-export const n31_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_1.jpg";
-export const n32_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_1_2x.jpg";
-export const n33_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/commercial_2x.jpg";
-export const n34_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/commercial.jpg";
-export const n35_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_sizes_main_1_2x.jpg";
-
-//
-
-export const n36_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest.jpg";
-export const n37_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_sizes_main_1.jpg";
-export const n38_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest_2x.jpg";
-export const n39_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest_large_2x.jpg";
-export const n40_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest_large.jpg";
-
-//
-
-export const n41_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow_2x.jpg";
-export const n42_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow_2x.jpg";
-export const n43_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow.jpg";
-export const n44_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_small.jpg";
-export const n45_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow_large_2x.jpg";
-
-//
-
-export const n46_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_meadow_2x.jpg";
-export const n47_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost_2x.jpg";
-export const n48_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_meadow.jpg";
-export const n49_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow_large.jpg";
-export const n50_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_frost_2x.jpg";
-
-//
-
-export const n51_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost.jpg";
-export const n52_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost_large_2x.jpg";
-export const n53_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_large_2x.jpg";
-export const n54_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_large.jpg";
-export const n55_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_frost.jpg";
-
-//
-
-export const n56_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_acorn_2x.jpg";
-export const n57_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_2x.jpg";
-export const n58_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost_large.jpg";
-export const n59_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark.jpg";
-export const n60_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_small_2x.jpg";
-
-//
-
-export const n61_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_large_2x.jpg";
-export const n62_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_small.jpg";
-export const n63_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_acorn.jpg";
-export const n64_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_3_2x.jpg";
-export const n65_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_large.jpg";
-
-//
-
-export const n66_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_2x.jpg";
-export const n67_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/certification_2x.jpg";
-export const n68_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_3.jpg";
-export const n69_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn.jpg";
-export const n70_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/certification.jpg";
-
-//
-
-export const n71_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_2_2x.jpg";
-export const n72_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_1_2x.jpg";
-export const n73_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_1.jpg";
-export const n74_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_2.jpg";
-export const n75_ =
-  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_small_2x.jpg";
-
-//
+// prettier-ignore
+export const imgs = {
+certification_2x:      "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/certification_2x.jpg",
+certification:         "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/certification.jpg",
+collection_1_2x:       "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_1_2x.jpg",
+collection_1:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_1.jpg",
+collection_2_2x:       "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_2_2x.jpg",
+collection_2:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_2.jpg",
+collection_3_2x:       "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_3_2x.jpg",
+collection_3:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/collection_3.jpg",
+color_acorn_2x:        "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_2x.jpg",
+color_acorn_acorn_2x:  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_acorn_2x.jpg",
+color_acorn_acorn:     "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_acorn.jpg",
+color_acorn_frost_2x:  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_frost_2x.jpg",
+color_acorn_frost:     "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_frost.jpg",
+color_acorn_large_2x:  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_large_2x.jpg",
+color_acorn_large:     "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_large.jpg",
+color_acorn_meadow_2x: "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_meadow_2x.jpg",
+color_acorn_meadow:    "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_meadow.jpg",
+color_acorn_small_2x:  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_small_2x.jpg",
+color_acorn_small:     "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn_small.jpg",
+color_acorn:           "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_acorn.jpg",
+color_bark_2x:         "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_2x.jpg",
+color_bark_large_2x:   "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_large_2x.jpg",
+color_bark_large:      "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_large.jpg",
+color_bark_small_2x:   "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_small_2x.jpg",
+color_bark_small:      "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark_small.jpg",
+color_bark:            "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_bark.jpg",
+color_frost_2x:        "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost_2x.jpg",
+color_frost_large_2x:  "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost_large_2x.jpg",
+color_frost_large:     "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost_large.jpg",
+color_frost:           "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_frost.jpg",
+color_meadow_2x:       "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow_2x.jpg",
+color_meadow_large_2x: "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow_large_2x.jpg",
+color_meadow_large:    "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow_large.jpg",
+color_meadow:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_meadow.jpg",
+color_nest_2x:         "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest_2x.jpg",
+color_nest_large_2x:   "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest_large_2x.jpg",
+color_nest_large:      "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest_large.jpg",
+color_nest:            "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_nest.jpg",
+color_sizes_main_1_2x: "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_sizes_main_1_2x.jpg",
+color_sizes_main_1:    "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/color_sizes_main_1.jpg",
+commercial_2x:         "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/commercial_2x.jpg",
+commercial:            "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/commercial.jpg",
+gallery_1_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_1_2x.jpg",
+gallery_1:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_1.jpg",
+gallery_2_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_2_2x.jpg",
+gallery_2:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_2.jpg",
+gallery_3_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_3_2x.jpg",
+gallery_3:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_3.jpg",
+gallery_4_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_4_2x.jpg",
+gallery_4:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_4.jpg",
+gallery_5_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_5_2x.jpg",
+gallery_5:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_5.jpg",
+gallery_6_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_6_2x.jpg",
+gallery_6:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_6.jpg",
+gallery_7_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_7_2x.jpg",
+gallery_7:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_7.jpg",
+gallery_8_2x:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_8_2x.jpg",
+gallery_8:             "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/gallery_8.jpg",
+home_banner_2x:        "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/home_banner_2x.jpg",
+home_banner:           "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/home_banner.jpg",
+residential_1_2x:      "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_1_2x.jpg",
+residential_1:         "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_1.jpg",
+residential_2_2x:      "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_2_2x.jpg",
+residential_2:         "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/residential_2.jpg",
+sample_acorn_2x:       "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_acorn_2x.png",
+sample_acorn:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_acorn.png",
+sample_bark_2x:        "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_bark_2x.png",
+sample_bark:           "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_bark.png",
+sample_frost_2x:       "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_frost_2x.png",
+sample_frost:          "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_frost.png",
+sample_meadow_2x:      "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_meadow_2x.png",
+sample_meadow:         "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_meadow.png",
+sample_nest_2x:        "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_nest_2x.png",
+sample_nest:           "https://res.cloudinary.com/bedrosians/image/upload/v1/assets/planx/sample_nest.png",
+};
+// 'color_meadow_2x' was two copies, so I deleted one
